565d5ceb7654a6797298dd7747f3158a2e8e470a
shikon <yonkon.ru@gmail.com>
2015-03-21 00:35:17 +0200

    images and core

---
+++ block_main.noncached.php
@@ -0,0 +1,21 @@
+<?php
+
+$properties = $modx->resource->getOne('Template')->getProperties();
+
+if(!empty($properties['tpl'])){
+    $tpl = $properties['tpl'];
+}
+else{
+    $tpl = 'block_main.tpl';
+}
+
+if ($modx->resource->cacheable != '1') {
+    $modx->smarty->caching = false;
+}
+
+
+$modx->smarty->compile_check = false;
+$modx->smarty->force_compile = true;
+
+
+return $modx->smarty->fetch("tpl/{$tpl}");
